b3f317d9473159fdfc0050e05f258454b2300fb4
ezeedev <fernandocristaldo21@hotmail.com>
2022-07-01 16:12:50 -0300

    app finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,53 @@
 import { useEffect, useState} from 'react'
 import Character from './components/Character'
-import './index.css'
+import '../src/index.css'
 
 function App() {
 
   const [characters, setCharacter] = useState([])
+  
   const URL = "https://fedeperin-harry-potter-api.herokuapp.com/db"
+  
+  
+  
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(URL);
       const data = await response.json();
-      setCharacter(data.personajes)
+      setCharacter(data.personajes);
     }
+
     fetchData()
   }
   , [])
 
+
+
   return (
 
-    <div className="grid grid-cols-2">
-      <h1 className='bg-black'>Potter</h1>
-      <p>
-        This is a simple app that will allow you to search for a Harry Potter character.
-      </p>
+    <div className="bg-slate-600">
+      <div className="container py-10 text-white text-center m-auto h-ful">
+        <h1 className=' font-bold text-3xl py-4'>Potter App</h1>
+        <p className=' text-3xl px-4 mb-24'>
+          This is a simple app that will allow you to search for a main character from Harry Potter.
+        </p>
+      
+        <div className="flex justify-center items-center text-center flex-wrap gap-14">
+          {
+          characters.map(character => {
+            return (
 
-      {
-      characters.map(character => {
-        return (
-          
-          <div key={character.id}>
-            <Character 
-              character = {character}
-            />
-         </div>
-         
-        )
-       })
-      }
+            <div key={character.id}>
+                <Character 
+                  character = {character}
+                />
+             </div>
 
+            )
+           })
+          }
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,16 +1,26 @@
 function Character( {character} ) {
+   
   return (
-    <div className="">
+    <div className="container max-w-2xl">
         <div>
-          <h2>{character.personaje}</h2>
-        </div>
+          <h2 className="mb-3 uppercase font-bold">
+            {
+              character.apodo
+            }
+          </h2>
+         <div className="">
+           <img
+            src={character.imagen} 
+            alt={character.apodo}
+            className="mb-3 w-[250px] h-[300px] object-fill rounded-lg border border-white"
+          />
+           <h4 className="font-bold text-xl">{character.casaDeHogwarts}</h4>
+         </div>
 
-        <div >
-          <img src={character.imagen} alt={character.personaje}/>
-          <h4>Casa: {character.casaDeHogwarts}</h4>
         </div>
   </div>
   )
 }
+
 
 export default Character
